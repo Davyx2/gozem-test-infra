e438a790809f394cd66354e64afc0537c660bcd3
SEIDOU <david.seidou@iviplay.com>
2023-11-25 04:46:51 +0100

    ingore user data

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 logdir=/var/log
-=
+
 logfile=${logdir}/_setup.log
 #-- docker
 sudo apt-get update -y 
@@ -8,18 +8,24 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh 
 sudo usermod -a -G docker $USER 
 sudo docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres:13
-#--- nginx
-sudo apt-get install nginx -y 
-sudo systemctl start nginx 
-sudo systemctl enable nginx 
-#--- certbot
-sudo apt install certbot python3-certbot-nginx -y 
 
 #--- docker compose
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+#------aws ecr --------------
+sudo apt-get install awscli amazon-ecr-credential-helper
+# Add credential helper to pull from ECR
+sudo mkdir -p ~/.docker 
+sudo chmod 0700 ~/.docker
+sudo touch ~/.docker/config.json
+sudo chown -R $USER:$USER .docker/
+sudo echo '{"credsStore": "ecr-login"}' >> ~/.docker/config.json
+
+sudo docker run  --name api  -d -p 80:8088 -e NODE_DOCKER_PORT=8088  -e DB_URL="mongodb+srv://manassehsuccess0:OH0HRop6zlTYKdOc@cluster0.doukqss.mongodb.net/?retryWrites=true&w=majority" 137975280244.dkr.ecr.eu-west-3.amazonaws.com/gozem-test
+
 #--- gitlab runner
-curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
-sudo apt-get install gitlab-runner
+#curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash#
+#sudo apt-get install gitlab-runner
--- module/template-instance/main.tf
+++ module/template-instance/main.tf
@@ -9,9 +9,6 @@ data "aws_subnet" "main" {
 resource "aws_eip" "main" {
   instance = aws_instance.main.id
   #vpc      = true
-  lifecycle {
-    prevent_destroy = true
-  }
   tags = {
     Name   = var.tags
   }
@@ -20,17 +17,15 @@ resource "aws_eip" "main" {
 resource "aws_eip_association" "main" {
   instance_id   = aws_instance.main.id
   allocation_id = aws_eip.main.id
-   lifecycle {
-    prevent_destroy = true
-  }
+  #  lifecycle {
+  #   prevent_destroy = true
+  # }
 } 
 
 resource "aws_key_pair" "main" {
   public_key = file(var.pubkey-file)
   key_name   = var.ami_key_pair_project
-  lifecycle {
-    prevent_destroy = true
-  }
+
 }
 
 resource "aws_instance" "main" {
@@ -38,6 +33,7 @@ resource "aws_instance" "main" {
   subnet_id              = data.aws_subnet.main.id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.main.key_name
+  iam_instance_profile   = aws_iam_instance_profile.profile.name
   vpc_security_group_ids = [var.security_group_id]
   user_data              = file("ec2-user-data.sh")
   user_data_replace_on_change = true
@@ -48,9 +44,9 @@ resource "aws_instance" "main" {
     volume_size = var.size_ebs
   }
   
-  lifecycle {
-    prevent_destroy = true
-  } 
+  # lifecycle {
+  #   prevent_destroy = true
+  # } 
   
   tags = {
     Name = var.tags
